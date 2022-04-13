53b3974add233ce4ea2fa055d68620f06228b4b2
Scott Evans <michealsevans20@gmail.com>
2022-04-12 19:34:16 -0500

    rendered the horned beasts

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -5,7 +5,7 @@
      return (
        <>
           <h2>{this.props.title}</h2>
-          <img url={this.props.imageUrl} alt={this.props.description} title={this.props.title} />
+          <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title} />
           <p>{this.props.description}</p>
        </>
      )
